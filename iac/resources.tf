4b4c05bb2f8d39dbe028023e0a8acd6d4da5126a
luis-doin-cko <132442360+luis-doin-cko@users.noreply.github.com>
2023-12-05 07:33:14 +0000

    create lambda function

--- iac/resources.tf
+++ iac/resources.tf
@@ -46,3 +46,120 @@ resource "aws_sqs_queue_policy" "event_sqs_policy" {
 }
 POLICY
 }
+
+resource "random_uuid" "bucket_random_id" {
+}
+
+# Create S3 bucket to store our application source code.
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket                = "${random_uuid.bucket_random_id.result}-dotnet-tf-bucket"
+  force_destroy         = true
+}
+
+data "archive_file" "lambda_archive" {
+  type = "zip"
+
+  source_dir  = "../EventListener/src/EventListener/bin/Release/net6.0/linux-x64/publish"
+  output_path = "EventListener.zip"
+}
+
+resource "aws_s3_object" "lambda_bundle" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "EventListener.zip"
+  source = data.archive_file.lambda_archive.output_path
+
+  etag = filemd5(data.archive_file.lambda_archive.output_path)
+}
+
+resource "aws_cloudwatch_log_group" "aggregator" {
+  name = "/aws/lambda/${aws_lambda_function.function.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_function_role" {
+  name = "FunctionIamRole_event-listener"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_function_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "function" {
+  function_name    = "event-listener"
+  s3_bucket        = aws_s3_bucket.lambda_bucket.id
+  s3_key           = aws_s3_object.lambda_bundle.key
+  runtime          = "dotnet6"
+  handler          = "EventListener::EventListener.Function::FunctionHandler"
+  source_code_hash = data.archive_file.lambda_archive.output_base64sha256
+  role             = aws_iam_role.lambda_function_role.arn
+  timeout          = 30
+}
+
+output "function_name" {
+  value = aws_lambda_function.function.function_name
+}
+
+resource "aws_iam_role_policy" "lambda_role_sqs_policy" {
+    name = "AllowSQSPermissions"
+    role = "${aws_iam_role.lambda_function_role.name}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "sqs:ChangeMessageVisibility",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:ReceiveMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_role_logs_policy" {
+    name = "LambdaRolePolicy"
+    role = "${aws_iam_role.lambda_function_role.name}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_event_source_mapping" "event_listener_event_source" {
+    event_source_arn = "${aws_sqs_queue.event_sqs.arn}"
+    enabled          = true
+    function_name    = "${aws_lambda_function.function.arn}"
+    batch_size       = 1
+}
