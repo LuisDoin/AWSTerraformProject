3e66573cb9244fbe4b75e3a9b3f80be7e2fc0186
luis-doin-cko <132442360+luis-doin-cko@users.noreply.github.com>
2023-12-06 15:30:36 +0000

    Create Lambda

--- iac/resources.tf
+++ iac/resources.tf
@@ -74,14 +74,14 @@ resource "aws_s3_bucket" "lambda_bucket" {
 data "archive_file" "lambda_archive" {
   type = "zip"
 
-  source_dir  = "../EventListener/src/EventListener/bin/Release/net6.0/linux-x64/publish"
-  output_path = "EventListener.zip"
+  source_dir  = "../EventListenerLambda/src/EventListenerLambda/bin/Release/net6.0/linux-x64/publish"
+  output_path = "EventListenerLambda.zip"
 }
 
 resource "aws_s3_object" "lambda_bundle" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
-  key    = "EventListener.zip"
+  key    = "EventListenerLambda.zip"
   source = data.archive_file.lambda_archive.output_path
 
   etag = filemd5(data.archive_file.lambda_archive.output_path)
@@ -102,7 +102,7 @@ resource "aws_lambda_function" "function" {
   s3_bucket        = aws_s3_bucket.lambda_bucket.id
   s3_key           = aws_s3_object.lambda_bundle.key
   runtime          = "dotnet6"
-  handler          = "EventListener::EventListener.Function::FunctionHandler"
+  handler          = "EventListenerLambda::EventListenerLambda.Function::FunctionHandler"
   source_code_hash = data.archive_file.lambda_archive.output_base64sha256
   role             = aws_iam_role.lambda_function_role.arn
   timeout          = 30
